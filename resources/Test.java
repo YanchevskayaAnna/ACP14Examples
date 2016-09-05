9537c3d97b05158548c2a068d62b8bf3a7b21e1a
Yanchevskaya Anna <yanchevskayaa@gmail.com>
2016-09-05 22:57:09 +0300

    add exeptions

---
+++ resources/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Anna on 04.09.2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test"));
+    }
+}
--- src/executeCommandFromJava/TestExecuteFromJava.java
+++ src/executeCommandFromJava/TestExecuteFromJava.java
@@ -4,40 +4,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Anna on 04.09.2016.
  */
+
 public class TestExecuteFromJava {
 
+    public static final String TEST_JAVA = "resources/Test.java";
+
     public static void main(String[] args) {
-        String out = executeCommand("javac " + "D:\\ACP14\\Test.java");
-        if (out.length() == 0) System.out.println("Compiled!");
-        else System.out.println("Bad code\n" + out);
-
-        System.out.println(out);
-    }
-
-    public static String executeCommand(String command) {
-
-        String output = "";
-        String error = "";
 
         try {
-            Process p;
-            p = Runtime.getRuntime().exec(command);
-            p.waitFor();
-            output = readAll(p.getInputStream());
-            error = readAll(p.getErrorStream());
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            executeCommand("javac " + TEST_JAVA);
+            System.out.println("Compiled!");
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
-
-        return error == ""?output:error;
     }
 
-  private static String readAll(InputStream is){
+    public static String executeCommand(String command) throws ExecutionException {
+
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            process.waitFor();
+            int resultCode = process.exitValue();
+            String output = readAll(process.getInputStream());
+            String error = readAll(process.getErrorStream());
+
+            if (resultCode != 0 || !error.isEmpty()) {
+                throw new ExecutionException("Bad code\n" + error, null);
+            }
+
+            return output;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    private static String readAll(InputStream is) {
         StringBuffer output = new StringBuffer();
         BufferedReader reader =
                 new BufferedReader(new InputStreamReader(is));
